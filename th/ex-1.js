e0b4326f36090dc23277d29cc87a4573686204fa
thanakorn <thanakorn.stsb@gmail.com>
2026-01-28 11:34:10 +0700

    feat: implement moveZeroes and romanToInt functions

--- th/ex-1.js
+++ th/ex-1.js
@@ -1,5 +1,16 @@
 const moveZeroes = function (nums) {
-  //Start coding here
+  let left = 0
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      let temp = nums[i]
+      nums[i] = nums[left]
+      nums[left] = temp
+      left++
+    }
+  }
+
+  return (nums)
 };
 
 const result1 = moveZeroes([0, 1, 0, 3, 12]);
--- th/ex-2.js
+++ th/ex-2.js
@@ -1,5 +1,24 @@
 const romanToInt = function (s) {
-  //Start coding here
+  const romanMap = {
+    I:1,
+    V:5,
+    X:10,
+    L:50,
+    C:100,
+    D:500,
+    M:1000
+  }
+  let result = 0
+
+  for (let i = 0; i < s.length; i++) {
+    if (romanMap[s[i]] < romanMap[s[i+1]]) {
+      result += romanMap[s[i+1]] - romanMap[s[i]]
+      i++
+    } else {
+      result += romanMap[s[i]]
+    }
+  } 
+  return result
 };
 
 const result1 = romanToInt("III"); // 3
